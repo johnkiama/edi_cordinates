15e9b17ba999395ca025068a6590675bf706bd99
John Kiama <kiamamaina@yahoo.com>
2023-05-09 10:25:45 +0300

    added new features

--- read_edi_cordinate.py
+++ read_edi_cordinate.py
@@ -1,14 +1,13 @@
 import os, sys
-
+####################################################################################################################
+#This script allows user to read location of individual/batch edi file and plot their location on QGIS or google map
+#how to use                                                    
+#read_edi_cordinate.py >> name_of_csv_file.csv
+#####################################################################################################################
 def editedi(indir):
- #   os.chdir(indir)
     edi = [ i for i in os.listdir(indir) if i.endswith(".edi")]
     return edi
-    # files = os.listdir(indir)
-    # for file in files:
-    #     if file.endswith(".edi"):
-    #         yield file
-
+  
 def display_files(path):
     my_files = editedi(path)
     for n in my_files:
@@ -18,21 +17,12 @@ def display_files(path):
             for lines in reader:
                 lines = lines.strip()
                 if lines.startswith("LAT"):
-                    latname, lat = lines.split("=")
-                    lines = n + " " + lat
-                if lines.startswith("LONG"):
-                   longname, long = lines.split("=")
-                   lines = " " + long
-                   my_cordinates = n, lat, long
-                   for i in my_cordinates:
-#                        print(long)
-                        print(i)
-#       
-              
+                    latname = lines.split("=")
+                    lat = latname[1]
+                if lines.startswith("LON"): #edi eported from winglink
+#                if lines.startswith("LONG"): #edi from mpower or mteditor
+                    longname = lines.split("=")
+                    long = longname[1]
+                    print(n, lat, long)
+#user to edit location of edi file 
 display_files("/home/seismics/MTPY-WINDOWS/PAKA_EDI")
-
-
-
-
-    
-
